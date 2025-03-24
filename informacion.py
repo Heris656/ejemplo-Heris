fe3060bfe2bd0f596e97bd4b30b2fb965e44228a
Heris6561131 <herissanchez542@gmail.com>
2025-03-24 08:06:35 -0400

    Modificacion y agregado comentario al archivo informacion.py

--- informacion.py
+++ informacion.py
@@ -5,6 +5,10 @@ while True:
         edad = int(input("Ingrese su edad: "))
         break
     except ValueError:
-        print("Por favor, ingrese un número válido para la edad.")
+        print("Porfa, ingresa tu edad en números.")
+        
+if edad < 18:
+    print(f"wenas, {nombre}! Eres menor de edad, disfruta tu juventud.")
 
-print(f"Hola, {nombre}! Tienes {edad} años.")
+elif edad >= 18:  # Se usa elif en lugar de otro if, si no se usa imprimiria los dos mensajes.
+    print(f"Wenas, {nombre}! ¿Como que tienes {edad} años? Jaja.")
