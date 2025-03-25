49f6edda3b03a578725c8382b4e431c52d8ece72
Heris6561131 <herissanchez542@gmail.com>
2025-03-25 10:39:36 -0400

    Haciendo algunos cambios

--- informacion.py
+++ informacion.py
@@ -8,7 +8,10 @@ while True:
         print("Porfa, ingresa tu edad en números.")
         
 if edad < 18:
-    print(f"wenas, {nombre}! Eres menor de edad, disfruta tu juventud.")
+    print(f"wenas, {nombre} Eres menor de edad, no debes pagar impuestos.")
 
 elif edad >= 18:  # Se usa elif en lugar de otro if, si no se usa imprimiria los dos mensajes.
-    print(f"Wenas, {nombre}! ¿Como que tienes {edad} años? Jaja.")
+    print(f"Wenas, {nombre} ¿Como que tienes {edad} años? Pagueee.")
+    
+elif edad == 18: 
+    print(f"Usted {nombre} inicia a paguar impuesto")
